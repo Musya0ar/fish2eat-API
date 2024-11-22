7d9150c6c939bbb48e6041b67c4031256f4745ad
Musya0ar <93309016+Musya0ar@users.noreply.github.com>
2024-11-23 05:09:20 +0700

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccountPath = "/secrets/firebase-adminsdk.json"; // Path where secret is mounted
+const serviceAccountPath = "/secrets/firebase-adminsdk.json";
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccountPath),
