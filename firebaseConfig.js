86cecb6c2d8d57d969e54cf8f3b76dc274edc555
Musya0ar <93309016+Musya0ar@users.noreply.github.com>
2024-11-22 21:55:44 +0700

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,12 +1,9 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../fish2eat-API/firebase-adminsdk.json");
-require("dotenv").config();
+const serviceAccountPath = "/secrets/firebase-adminsdk.json"; // Path where secret is mounted
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
+    credential: admin.credential.cert(serviceAccountPath),
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
 });
 
 const db = admin.firestore();
-
-module.exports = { admin, db };
